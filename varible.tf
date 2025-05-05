7e99d0ddac0eb2f234f670a786f251e5e942f215
Juhi Choudhary <115893427+Juhi5863@users.noreply.github.com>
2025-05-05 17:24:54 +0530

    Create varible.tf

---
+++ varible.tf
@@ -0,0 +1,9 @@
+variable "my_ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "my_instance_type" {
+  description = "The instance type to use"
+  type        = string
+}
